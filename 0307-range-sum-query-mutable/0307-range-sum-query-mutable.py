30660c66f7b9534ca9cc8d257cc243fd011d8df8
Quan nguyen <binguyen251013@gmail.com>
2026-07-07 11:33:05 +0700

    Time: 387 ms (75.74%), Space: 38 MB (85.44%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -1,32 +1,28 @@
 class NumArray:
-
     def __init__(self, nums: List[int]):
         self.arr = [0] + nums
         self.n = len(nums)
         self.fenwick = [0] * (self.n + 1)
-        for self.i in range(1, self.n + 1):
-            self.upd(self.i, self.arr[self.i])
         
-    def upd(self, idx: int, val: int) -> None:
+        for i in range(1, self.n + 1):
+            self.upd(i, self.arr[i])
+        
+    def upd(self, idx: int, delta: int) -> None:
         while idx <= self.n:
-            self.fenwick[idx] += val
+            self.fenwick[idx] += delta
             idx += idx & (-idx)
 
     def update(self, index: int, val: int) -> None:
-        self.change = val - self.arr[index + 1]
+        change = val - self.arr[index + 1]
         self.arr[index + 1] = val
-        self.upd(index + 1, self.change)
+        self.upd(index + 1, change)
+        
     def pre_i(self, i: int) -> int:
-        self.total = 0
+        total = 0
         while i > 0:
-            self.total += self.fenwick[i]
+            total += self.fenwick[i]
             i -= i & (-i)
-        return self.total
+        return total
+        
     def sumRange(self, left: int, right: int) -> int:
         return self.pre_i(right + 1) - self.pre_i(left)
-
-
-# Your NumArray object will be instantiated and called as such:
-# obj = NumArray(nums)
-# obj.update(index,val)
-# param_2 = obj.sumRange(left,right)
