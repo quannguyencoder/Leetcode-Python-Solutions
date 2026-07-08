09ed79aedadbdf637ac2002f6ee5403653c7c0d8
Quan nguyen <binguyen251013@gmail.com>
2026-07-08 11:17:41 +0700

    07-08-2026 Array Medium 0204-count-primes 0 ms (100%) 277 MB (6.23%) .py

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -1,15 +1,13 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        pref_sum = [0] * (n + 1)
-        cnt = 0
-        sieve = bytearray([1]) * (n + 1)
-        if n > 2:
-            sieve[2] = 1
-            cnt += 1
-            sieve[0] = sieve[1] = 0
-            sieve[4: n: 2] = bytearray([0]) * len(sieve[4: n: 2])
-            for i in range(3, n, 2):
-                if sieve[i]:
-                    cnt += 1
-                    sieve[i * i: n: i * 2] = bytearray([0]) * len(sieve[i * i: n: i * 2])
-        return cnt
+        if n == 0:
+            return 0
+        return self.pref_sum[n - 1]
+    N = 5 * pow(10, 6) + 1
+    sieve = [1] * (N + 1)
+    sieve[0] = sieve[1] = 0
+    sieve[4: N: 2] = [0] * len(sieve[4: N: 2])
+    for i in range(3, isqrt(N), 2):
+        if sieve[i]:
+            sieve[i * i: N: i * 2] = [0] * len(sieve[i * i: N: i * 2])
+    pref_sum = list(accumulate(sieve))
