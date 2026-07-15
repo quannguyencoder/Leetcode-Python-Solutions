1c6d4becb5657d539122ca2ebe0c5aa8c951f0b7
Quan nguyen <binguyen251013@gmail.com>
2026-07-15 20:23:09 +0700

    07-15-2026 2 ms (44.48%) 19.2 MB (53.03%) Math

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -1,5 +1,5 @@
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
-        sum_even = n * (n + 1)
         sum_odd = n ** 2
-        return gcd(sum_even, sum_odd)
+        sum_even = n * (n + 1)
+        return gcd(sum_odd, sum_even)
