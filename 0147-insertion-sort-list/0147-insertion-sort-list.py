e696b009b2d11a53f8c617ca85cd820052b85f4d
Quan nguyen <binguyen251013@gmail.com>
2026-07-18 21:18:06 +0700

    07-18-2026 3 ms (96.12%) 21.3 MB (29.18%) Linked List

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        nums = []
+        while cur:
+            nums.append(cur.val)
+            cur = cur.next
+        nums.sort()
+        cur = head
+        i = 0
+        while cur:
+            cur.val = nums[i]
+            cur = cur.next
+            i += 1
+        return head
