abbb1d0f940f4527ab692fa5ee3bbe207d6f70d0
Quan nguyen <binguyen251013@gmail.com>
2026-07-21 10:22:17 +0700

    07-21-2026 287 ms (99.07%) 21.5 MB (41.12%) String

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
@@ -1,26 +1,8 @@
 class Solution:
     def maxActiveSectionsAfterTrade(self, s: str) -> int:
-        nums = [0]
-        for num in s:
-            if num == '0':
-                if nums[-1] < 0:
-                    nums[-1] += -1
-                else:
-                    nums.append(-1)
-            else:
-                if nums[-1] > 0:
-                    nums[-1] += 1
-                else:
-                    nums.append(1)
-        nums = nums[1:]
-        prefix_sum = [0]
-        for num in nums:
-            prefix_sum.append(prefix_sum[-1] + num) if num > 0 else prefix_sum.append(prefix_sum[-1])
-        res = prefix_sum[-1]
-        for i in range(1, len(nums) - 1):
-            if nums[i] > 0:
-                cur = abs(nums[i - 1]) + nums[i] + abs(nums[i + 1])
-                suffix = prefix_sum[-1] - prefix_sum[i + 1]
-                prefix = prefix_sum[i-1]
-                res = max(res, cur + suffix + prefix)
-        return res
+        ones = s.count('1')
+        zeros = [len(sub_0) for sub_0 in s.split('1') if sub_0]
+        if len(zeros) < 2:
+            return ones
+        max_zeros = max(zeros[i] + zeros[i + 1] for i in range(len(zeros) - 1))
+        return ones + max_zeros
