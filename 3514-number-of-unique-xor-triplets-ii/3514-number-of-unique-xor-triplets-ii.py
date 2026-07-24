c81102a3011470b20e11fef9758f7aeb6496af8f
Quan nguyen <binguyen251013@gmail.com>
2026-07-24 12:28:46 +0700

    07-24-2026 5864 ms (64.81%) 19.4 MB (92.59%) Array

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.py
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def uniqueXorTriplets(self, nums: List[int]) -> int:
-        uniqeue_num = list(set(nums))
-        cur = uniqeue_num[:]
+        uniqeue_num = set(nums)
+        cur = uniqeue_num
         for _ in range(2):
             res = set()
-            for i in range(len(uniqeue_num)):
-                for j in range(len(cur)):
-                    res.add(uniqeue_num[i] ^ cur[j])
+            for i in uniqeue_num:
+                for j in cur:
+                    res.add(i ^ j)
             cur = list(res)
         return len(res)
