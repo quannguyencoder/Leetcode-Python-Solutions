49c2210c4da29cfa8354c7a6de31ba353857a908
Quan nguyen <binguyen251013@gmail.com>
2026-08-05 08:44:45 +0700

    08-05-2026 271 ms (76.51%) 146.2 MB (27.27%) Depth-First Search

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.py
@@ -0,0 +1,20 @@
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+        suspicious = set()
+        grid = [[] for _ in range(n)]
+        for u, v in invocations:
+            grid[u].append(v)
+        def dfs(cur):
+            suspicious.add(cur)
+            for f1 in grid[cur]:
+                if f1 not in suspicious:
+                    dfs(f1)
+        dfs(k)
+        clean = []
+        for u, v in invocations:
+            if u not in suspicious and v in suspicious:
+                return [i for i in range(n)]
+        for i in range(n):
+            if i not in suspicious:
+                clean.append(i)
+        return clean
